be91e301a61875f08af5c4ea8e498f38646205d8
jenkins <jenkins@kurento.org>
2015-07-03 09:18:55 +0000

    Generated code from kms-chroma efeb62aed097acec2aa2c9c37ef1432bf186f3c6

---
+++ Gruntfile.js
@@ -0,0 +1,140 @@
+/*
+ * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+
+module.exports = function(grunt)
+{
+  var DIST_DIR = 'dist';
+
+  var pkg = grunt.file.readJSON('package.json');
+
+  const PKG_BROWSER = 'lib/browser.js';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: pkg,
+
+    // Plugins configuration
+    clean:
+    {
+      'doc':     '<%= jsdoc.all.dest %>',
+      'browser': DIST_DIR
+    },
+
+    bower:
+    {
+      TOKEN:      process.env.TOKEN,
+      repository: 'Kurento/kurento-module-chroma-js'
+    },
+
+    // Generate documentation
+    jsdoc:
+    {
+      all:
+      {
+        src: [
+          'README.md',
+          'lib/**/*.js',
+          'test/*.js'
+        ],
+        dest: 'doc/jsdoc'
+      }
+    },
+
+    // Generate browser versions and mapping debug file
+    browserify:
+    {
+      options: {
+        alias:    ['.:<%= pkg.name %>'],
+        external: [
+          'es6-promise',
+          'inherits',
+          'kurento-client',
+          'promisecallback'
+        ]
+      },
+
+      'standard':
+      {
+        src:  PKG_BROWSER,
+        dest: DIST_DIR+'/<%= pkg.name %>.js'
+      },
+
+      'minified':
+      {
+        src:  PKG_BROWSER,
+        dest: DIST_DIR+'/<%= pkg.name %>.min.js',
+
+        options:
+        {
+          browserifyOptions: {
+            debug: true
+          },
+          plugin: [
+            ['minifyify',
+             {
+               compressPath: DIST_DIR,
+               map:          '<%= pkg.name %>.map',
+               output:       DIST_DIR+'/<%= pkg.name %>.map'
+             }]
+          ]
+        }
+      }
+    },
+
+    // Generate bower.json file from package.json data
+    sync:
+    {
+      bower:
+      {
+        options:
+        {
+          sync: [
+            'name', 'description', 'license', 'keywords', 'homepage',
+            'repository'
+          ],
+          overrides: {
+            authors: (pkg.author ? [pkg.author] : []).concat(pkg.contributors || []),
+            ignore: ['doc/', 'lib/', 'Gruntfile.js', 'package.json'],
+            main: DIST_DIR+'/<%= pkg.name %>.js'
+          }
+        }
+      }
+    },
+
+    shell:
+    {
+      // Publish / update package info in Bower
+      bower: {
+        command: [
+          'curl -X DELETE "https://bower.herokuapp.com/packages/<%= pkg.name %>?auth_token=<%= bower.TOKEN %>"',
+          'node_modules/.bin/bower register <%= pkg.name %> <%= bower.repository %>',
+          'node_modules/.bin/bower cache clean'
+        ].join('&&')
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-npm2bower-sync');
+  grunt.loadNpmTasks('grunt-shell');
+
+  // Alias tasks
+  grunt.registerTask('default', ['clean', 'jsdoc', 'browserify', 'sync:bower']);
+  grunt.registerTask('bower',   ['shell:bower']);
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+kurento-module-chroma
+============
+Chroma Detector Filter
+
+This repository host the descriptor file for the [kurento] module chroma.
+
+[kurento]: http://www.kurento.org
